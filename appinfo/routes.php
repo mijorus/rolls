bf2b854233044c45add62bcfdda4ba2b269056ea
Lorenzo Paderi <lorenzo@mijorus.it>
2024-07-10 15:06:57 +0200

    added route for delete method

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -7,10 +7,11 @@ return [
 		['name' => 'page#index', 'url' => '/watch', 'verb' => 'GET', 'postfix' => 'watch'],
 		['name' => 'page#index', 'url' => '/watch/{path}', 'verb' => 'GET', 'postfix' => 'watchroll'],
 
-		['name' => 'rollapi#index',             'url' => '/api/hello', 'verb' => 'GET'],
-		['name' => 'rollapi#get_upload_path',   'url' => '/api/upload_path', 'verb' => 'GET'],
-		['name' => 'rollapi#create_roll',       'url' => '/api/rolls', 'verb' => 'POST'],
-		['name' => 'rollapi#get_rolls',       	'url' => '/api/rolls', 'verb' => 'GET'],
+		['name' => 'rollapi#index', 'url' => '/api/hello', 'verb' => 'GET'],
+		['name' => 'rollapi#get_upload_path', 'url' => '/api/upload_path', 'verb' => 'GET'],
+		['name' => 'rollapi#create_roll', 'url' => '/api/rolls', 'verb' => 'POST'],
+		['name' => 'rollapi#get_rolls',	'url' => '/api/rolls', 'verb' => 'GET'],
+		['name' => 'rollapi#delete_roll',	'url' => '/api/rolls', 'verb' => 'DELETE'],
 	],
 
 ];

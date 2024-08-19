a6b5fcaa5b49846059632a95dca611989b99b898
Lorenzo Paderi <39067225+mijorus@users.noreply.github.com>
2024-08-19 12:46:13 +0200

    wip

--- release.sh
+++ release.sh
@@ -12,5 +12,17 @@ tar -czf release.tar.gz \
 	composer.json \
 	composer.lock \
 	README.md
+
+mkdir rolls
+cp release.tar.gz rolls/
+
+cd rolls
+tar -zxf release.tar.gz
+rm release.tar.gz
+
+cd ..
+tar -czf release.tar.gz rolls
+
+rm -r rolls
 	
 openssl dgst -sha512 -sign ~/.nextcloud/certificates/rolls.key ./release.tar.gz | openssl base64 > release.sign
